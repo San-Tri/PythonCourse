17e4421a7af8cc0118ab1a04e80c6d9c77cf4639
San-Tri <1473024@senati.pe>
2023-02-27 16:06:34 -0500

    tipos de datos

---
+++ 2.TiposDeDatos.py
@@ -0,0 +1,12 @@
+# dato tipo string (cadenas de texto):
+"string"
+'string'
+"""string"""
+'''string'''
+# dato tipo int (números enteros):
+40 , 2 , 13 , 100
+# dato tipo float (números decimales):
+40.2 , 1.4 , 3.0
+# dato tipo bool (valores booleanos):
+True
+False
